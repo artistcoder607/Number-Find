2b7202a86d1423f30b8b61e6ddc2c374b9ffbb01
artistcoder607 <shahzaibcoder607@gmail.com>
2024-10-24 20:02:24 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+
+let userNum = prompt("Bhai apna number likho, 0 ki jagah +92 likhna");
+
+// Slice karne ki zarurat nahi hai agar sirf check karna hai
+if (userNum.startsWith("+92310") || userNum.startsWith("+92311") || userNum.startsWith("+92312") || userNum.startsWith("+92313") || userNum.startsWith("+92314") || userNum.startsWith("+92315") || userNum.startsWith("+92316") || userNum.startsWith("+92317") || userNum.startsWith("+92318") || userNum.startsWith("+92319")) { // Check if input starts with +92340
+    alert("Telenor");
+}
+
+ if (userNum.startsWith("+92300") || userNum.startsWith("+92301") || userNum.startsWith("+92302") || userNum.startsWith("+92303") || userNum.startsWith("+92304") || userNum.startsWith("+92305") || userNum.startsWith("+92306") || userNum.startsWith("+92307") || userNum.startsWith("+92308") || userNum.startsWith("+92309")) {
+    alert("Jazz");
+}
+
+if (userNum.startsWith("+92320") || userNum.startsWith("+92321") || userNum.startsWith("+92322") || userNum.startsWith("+92323") || userNum.startsWith("+92324") || userNum.startsWith("+92325") || userNum.startsWith("+92326") || userNum.startsWith("+92327") || userNum.startsWith("+92328") || userNum.startsWith("+92329")) {
+    alert ("Zong")
+}
+
+if (userNum.startsWith("+92330") || userNum.startsWith("+92331") || userNum.startsWith("+92332") || userNum.startsWith("+92333") || userNum.startsWith("+92334") || userNum.startsWith("+92335") || userNum.startsWith("+92336") || userNum.startsWith("+92337") || userNum.startsWith("+92338") || userNum.startsWith("+92339")) {
+    alert ("Ufone")
+}
